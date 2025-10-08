a1dfc7e8643c69bdeb809a2bcdd9fe35b090fad5
damnthonyy <mahassadijoseph@gmail.com>
2025-10-08 02:29:58 +0200

    lists & tulpes

---
+++ ListTulpes/index.py
@@ -0,0 +1,9 @@
+print("----------------------------------------")
+
+fruits = ["pomme", "banane", "cerise","orange"]
+
+for x in fruits:
+    print(f"{x} \n")
+
+
+print("----------------------------------------")
